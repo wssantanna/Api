528c8cdb1dc6a7afcbcba1f5cb685136d7cd8c62
Willian Sant Anna <willian.santanna@nextcoders.com.br>
2024-07-16 16:45:17 -0300

    implementei o mapeamento do banco de dados

---
+++ Contexts/LojaDbContext.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Contexts 
+{
+    public class LojaDbContext : DbContext
+    {
+        public LojaDbContext(DbContextOptions<LojaDbContext> configuracoes) : base (configuracoes) 
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Credencial> Credenciais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelador)
+        {
+            // 1:N
+            // HasOne   =   1
+            // HasMany  =   N
+            // WithOne  =   1
+            // WithMany =   N
+            modelador.Entity<Usuario>()
+                .HasOne(usuario => usuario.Endereco)
+                .WithMany()
+                .HasForeignKey(usuario => usuario.EnderecoId);
+            
+            // 1:1
+            modelador.Entity<Usuario>()
+                .HasOne(usuario => usuario.Credencial)
+                .WithOne(credencial => credencial.Usuario)
+                .HasForeignKey<Credencial>(credencial => credencial.UsuarioId);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Contexts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddDbContext<LojaDbContext>(configuracao => configuracao.UseInMemoryDatabase("db_loja"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minha API", Version = "v1" });
